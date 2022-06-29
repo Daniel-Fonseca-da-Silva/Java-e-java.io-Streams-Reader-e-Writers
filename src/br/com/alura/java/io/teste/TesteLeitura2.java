a3e123ac62afb33161b07416e54fc6bac73c4177
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-06-29 00:22:05 -0300

    Parseando arquivo

--- src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -1,6 +1,7 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -12,6 +13,23 @@ public class TesteLeitura2 {
 		while(scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
 			System.out.println(linha);
+
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.UK);
+			linhaScanner.useDelimiter(",");
+			
+			String valor1 = linhaScanner.next();
+			Integer valor2 = linhaScanner.nextInt();
+			Integer valor3 = linhaScanner.nextInt();
+			String valor4 = linhaScanner.next();
+			Double valor5 = linhaScanner.nextDouble();
+			
+			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+			
+			linhaScanner.close();
+//			String[] valores = linha.split(",");
+//			System.out.println(valores[3]);
+			
 		}
 		
 		scanner.close();
