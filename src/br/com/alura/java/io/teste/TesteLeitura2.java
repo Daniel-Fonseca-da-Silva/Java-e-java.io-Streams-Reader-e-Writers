9787e18126dc26404118f98f57fd3ee70f6cc3ac
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-06-29 22:28:56 -0300

    Formatação de valores

--- src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -12,19 +12,21 @@ public class TesteLeitura2 {
 		
 		while(scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
-			System.out.println(linha);
+//			System.out.println(linha);
 
 			Scanner linhaScanner = new Scanner(linha);
 			linhaScanner.useLocale(Locale.UK);
 			linhaScanner.useDelimiter(",");
 			
-			String valor1 = linhaScanner.next();
-			Integer valor2 = linhaScanner.nextInt();
-			Integer valor3 = linhaScanner.nextInt();
-			String valor4 = linhaScanner.next();
-			Double valor5 = linhaScanner.nextDouble();
+			String tipoConta = linhaScanner.next();
+			Integer agencia = linhaScanner.nextInt();
+			Integer numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			Double saldo = linhaScanner.nextDouble();
 			
-			System.out.println(valor1 + valor2 + valor3 + valor4 + valor5);
+			String valorFormatado = String.format(new Locale("pt", "BR"), "%s - %04d-%08d, %20s: %010.2fR$ %n",
+			tipoConta, agencia, numero, titular, saldo);
+			System.out.println(valorFormatado);
 			
 			linhaScanner.close();
 //			String[] valores = linha.split(",");
